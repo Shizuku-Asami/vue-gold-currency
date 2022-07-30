56779bdd7c32013826f9a4002e1dd8241f69d44c
Shizuku-Asami <xv.chronos@gmail.com>
2022-07-30 18:34:24 +0300

    add MetalCurrency Model

---
+++ src/models/metalcurrency.ts
@@ -0,0 +1,21 @@
+export interface MetalCurrency {
+  timestamp: number;
+  metal: string;
+  currency: string;
+  exchange: string;
+  symbol: string;
+  prev_close_price: number;
+  open_price: number;
+  low_price: number;
+  high_price: number;
+  open_time: number;
+  ch: number;
+  chp: number;
+  ask: number;
+  bid: number;
+  price_gram_24k: number;
+  price_gram_22k: number;
+  price_gram_21k: number;
+  price_gram_20k: number;
+  price_gram_18k: number;
+}
